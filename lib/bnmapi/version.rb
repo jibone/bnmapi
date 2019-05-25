4d1cace1f47e642df77da7a0ee73654beb7f7894
J Shamsul Bahri (jibone)) <jibone@gmail.com>
2019-05-26 04:12:41 +0800

    Initial commit for RDoc

--- lib/bnmapi/version.rb
+++
@@ -1,7 +0,0 @@
-module BnmAPI
-
-  ##
-  # Current vversion: 0.2.0
-
-  VERSION = '0.2.0'
-end
--- spec/bnmapi_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe BnmAPI do
-  it "has a version number" do
-    expect(BnmAPI::VERSION).not_to be nil
-  end
-end
